de8081e75352d715aad708463c57c38372ae422e
Vadim Pchelintsev <vadim.1.95@mail.ru>
2025-01-07 01:14:46 +0300

    правки по модели Transaction:
    1. добавил связь с user
    2. добавил поле created_at

--- models/transaction.py
+++ models/transaction.py
@@ -1,5 +1,9 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import Float, Enum
+from datetime import datetime
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import Float, Enum, DateTime, ForeignKey, Integer
+from sqlalchemy.sql import func
+
 from . import Base
 import enum
 
@@ -12,9 +16,13 @@ class TransactionStatus(enum.Enum):
 
 
 class Transaction(Base):
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"), nullable=False)
     amount: Mapped[float] = mapped_column(Float, nullable=False)
     commission: Mapped[float] = mapped_column(Float, nullable=False)
     status: Mapped[TransactionStatus] = mapped_column(Enum(TransactionStatus), default=TransactionStatus.PENDING)  # статус
+    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now(), nullable=False)
+
+    user = relationship("User", back_populates="transactions")
 
     def __repr__(self):
         return f"<Transaction(id={self.id}, amount={self.amount}, commission={self.commission}, status={self.status})>"
