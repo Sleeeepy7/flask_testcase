5715d2d2b0fbb395f09ee869966d1bc9a75ee7c2
Vadim Pchelintsev <vadim.1.95@mail.ru>
2025-01-07 01:12:42 +0300

    добавил swagger + рег admin и api

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flasgger import Swagger
 
 from core.config import settings
 
 from cli.create_admin import create_admin
+from admin.views import admin_bp
+from api.routes import api_bp
 
 
 def create_app():
@@ -10,7 +13,14 @@ def create_app():
     app.config["SECRET_KEY"] = settings.app.secret_key
     app.config["DEBUG"] = settings.app.debug
 
+    Swagger(app)
+
+    # CLI commands
     app.cli.add_command(create_admin)
+
+    # routes
+    app.register_blueprint(admin_bp, url_prefix="/admin")
+    app.register_blueprint(api_bp, url_prefix="/api")
 
     return app
 
